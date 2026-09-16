b41c313a7c6da166613e4cde94b6d4204337d72f
Mona <monabeydoun@gmail.com>
2026-09-16 20:14:16 +0300

    lock down CORS to production frontend URL

--- WebDev-L3-Pizzelo/server/server.js
+++ WebDev-L3-Pizzelo/server/server.js
@@ -16,7 +16,9 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
